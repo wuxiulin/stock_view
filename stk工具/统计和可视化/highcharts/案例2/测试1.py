1fa3e8a4652f595a49b41a3666b56e9961a54f84
wuxiulin <1048217547@qq.com>
2023-11-26 21:36:07 +0800

    “曲线多条”

--- stk工具/统计和可视化/highcharts/案例2/测试1.py
+++ stk工具/统计和可视化/highcharts/案例2/测试1.py
@@ -10,9 +10,34 @@ from datetime import datetime
 # 准备数据，这里使用一个简单的列表作为示例
 #dynamic_data = [1, 2, 3, 4, 5]
 
-def get_chart(dynamic_data):
+def get_chart(dynamic_data,name):
 
 	# 读取模板文件
+	# with open('template.html', 'r', encoding='utf-8') as template_file:
+	#     template_content = template_file.read()
+
+	# # 创建 Jinja2 模板对象
+	# template = Template(template_content)
+
+	# # 使用模板渲染 HTML，传递动态数据
+	# #在上面的代码中，dynamic_data 是一个包含你希望替换的动态数据的列表。
+	# #模板中的 {{ data | tojson | safe }} 部分将 Python 中的数据转换为 JSON 格式，并嵌入到 JavaScript 代码中。
+	# #html_output = template.render(data=dynamic_data)
+	# html_output = template.render(data=dynamic_data )
+	# # 将生成的 HTML 写入文件
+	# output_file_path='index.html'
+	# with open(output_file_path, 'w', encoding='utf-8') as output_file:
+	#     output_file.write(html_output)
+
+
+
+	#运行这个 Python 脚本，它将生成一个 HTML 文件（比如 output.html），其中包含动态数据的 JavaScript 代码。打开这个 HTML 文件，
+	#你应该能够看到 Highcharts 图表，其中的曲线数据是你在 Python 中指定的动态数据。
+
+
+
+
+
 	with open('template.html', 'r', encoding='utf-8') as template_file:
 	    template_content = template_file.read()
 
@@ -31,8 +56,7 @@ def get_chart(dynamic_data):
 
 
 
-	#运行这个 Python 脚本，它将生成一个 HTML 文件（比如 output.html），其中包含动态数据的 JavaScript 代码。打开这个 HTML 文件，
-	#你应该能够看到 Highcharts 图表，其中的曲线数据是你在 Python 中指定的动态数据。
+
 
 
 	# 用默认浏览器打开生成的 HTML 文件
@@ -72,25 +96,43 @@ if __name__ == '__main__':
 	# 			 }
 	# }
 	print(a)
-	print('*#######################################################')
-	out=[]
-	for i in range(len(a['date'])):
+	#print('*#######################################################')
+	# out=[]
+	# for i in range(len(a['date'])):
 
-		print('****************************************')
-		day=a['date'][i]
-		print(day,i)
-		print('****************************************')
-		timestamp =(datetime.strptime(day, "%Y-%m-%d")).timestamp()*1000#转为linux时间
-		try:
-			out.append([timestamp,a['line'][str(0)][i]])
-		except Exception as e:
-			print(a['line'])
-			print(a['line'][str(0)])
-			print(e)
-		
-	print(out)
+	# 	print('****************************************')
+	# 	day=a['date'][i]
+	# 	print(day,i)
+	# 	print('****************************************')
+	# 	timestamp =(datetime.strptime(day, "%Y-%m-%d")).timestamp()*1000#转为linux时间
+	# 	try:
+	# 		out.append([timestamp,a['line'][str(0)][i]])
+	# 	except Exception as e:
+	# 		print(a['line'])
+	# 		print(a['line'][str(0)])
+	# 		print(e)
+	dydata={}
+	for key in range(9):
+		out=[]
+		for i in range(len(a['date'])):
 
-	get_chart(out)
+			# print('****************************************')
+			day=a['date'][i]
+			# print(day,i)
+			# print('****************************************')
+			timestamp =(datetime.strptime(day, "%Y-%m-%d")).timestamp()*1000#转为linux时间
+			try:
+				out.append([timestamp,a['line'][str(key)][i]])
+			except Exception as e:
+				print(a['line'])
+				print(a['line'][str(key)])
+				print(e)
+	
+		dydata[str(key)+"板"]=out
+	print("PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP")
+	print(dydata)
+
+	get_chart(dydata,"证券")
 	#print(a)
 
 	 
